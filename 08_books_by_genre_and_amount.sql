e1db8fc88da40e668a3e2e54aa91ab45ee91423c
MariyaGvozdetskaya <m.kalyackina@yandex.ru>
2025-04-12 12:19:43 +0300

    Update 08_books_by_genre_and_amount.sql

--- 08_books_by_genre_and_amount.sql
+++ 08_books_by_genre_and_amount.sql
@@ -1,6 +1,7 @@
--- Книги (>8 экз.) по убыванию цены
-SELECT title, name_genre, price
-FROM genre 
-INNER JOIN book ON genre.genre_id=book.genre_id
-WHERE amount>8
-ORDER BY price DESC;
+Вывести название, жанр и цену тех книг, количество которых больше 8, в отсортированном по убыванию цены виде.
+
+  SELECT title, name_genre, price
+  FROM genre 
+  INNER JOIN book ON genre.genre_id=book.genre_id
+  WHERE amount>8
+  ORDER BY price DESC;
