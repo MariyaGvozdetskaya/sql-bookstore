20efa30ff4f00c9583189c7dc5fd5a982afdba37
MariyaGvozdetskaya <m.kalyackina@yandex.ru>
2025-04-12 12:19:13 +0300

    Update 07_books_cheaper_than_min.sql

--- 07_books_cheaper_than_min.sql
+++ 07_books_cheaper_than_min.sql
@@ -1,8 +1,9 @@
--- Книги, цена которых меньше минимальной цены у любого автора
-SELECT author, title, price
-FROM book
-WHERE price < ANY (
-        SELECT MIN(price) 
-        FROM book 
-        GROUP BY author 
-);
+Вывести информацию о книгах (автор, название, цена), цена которых меньше самой большой из минимальных цен, вычисленных для каждого автора.
+
+        SELECT author, title, price
+        FROM book
+        WHERE price < ANY (
+                SELECT MIN(price) 
+                FROM book 
+                GROUP BY author 
+                                );
