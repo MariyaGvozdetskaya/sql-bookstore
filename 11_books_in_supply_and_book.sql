a3e16f4c8befd7e40653ba13100cf5790290e392
MariyaGvozdetskaya <m.kalyackina@yandex.ru>
2025-04-12 12:23:42 +0300

    Update 11_books_in_supply_and_book.sql

--- 11_books_in_supply_and_book.sql
+++ 11_books_in_supply_and_book.sql
@@ -1,7 +1,10 @@
--- Общие книги в book и supply с одинаковой ценой
-SELECT book.title AS Название, name_author AS Автор, book.amount + supply.amount AS Количество
-FROM author 
-INNER JOIN book USING (author_id)   
-INNER JOIN supply ON book.title = supply.title 
+Если в таблицах supply  и book есть одинаковые книги, которые имеют равную цену,  вывести их название и автора.
+Посчитать общее количество экземпляров книг в таблицах supply и book. 
+Столбцы назвать Название, Автор  и Количество.
+
+  SELECT book.title AS Название, name_author AS Автор, book.amount + supply.amount AS Количество
+  FROM author 
+  INNER JOIN book USING (author_id)   
+  INNER JOIN supply ON book.title = supply.title 
                  AND author.name_author = supply.author 
                  AND book.price = supply.price;
