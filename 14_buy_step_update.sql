ea09cbcd0ec87bf0c69bbba730e2dba52a4581e3
MariyaGvozdetskaya <m.kalyackina@yandex.ru>
2025-04-12 12:31:29 +0300

    Update 14_buy_step_update.sql

--- 14_buy_step_update.sql
+++ 14_buy_step_update.sql
@@ -1,12 +1,13 @@
--- Завершение этапа 'Оплата' и начало 'Упаковка' для заказа 5
-UPDATE buy_step 
-SET date_step_end = '2020-04-13'
-WHERE step_id = (
-    SELECT step_id FROM step WHERE name_step = 'Оплата'
-) AND buy_id = 5;
+Завершить этап «Оплата» для заказа с номером 5, вставив в столбец date_step_end дату 13.04.2020, и начать следующий этап («Упаковка»), задав в столбце date_step_beg для этого этапа ту же дату.
 
-UPDATE buy_step 
-SET date_step_beg = '2020-04-13'
-WHERE step_id = (
-    SELECT step_id + 1 FROM step WHERE name_step = 'Оплата'
-) AND buy_id = 5;
+    UPDATE buy_step 
+    SET date_step_end = '2020-04-13'
+    WHERE step_id = (
+            SELECT step_id FROM step WHERE name_step = 'Оплата'
+        ) AND buy_id = 5;
+
+    UPDATE buy_step 
+    SET date_step_beg = '2020-04-13'
+    WHERE step_id = (
+            SELECT step_id + 1 FROM step WHERE name_step = 'Оплата'
+        ) AND buy_id = 5;
